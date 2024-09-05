9b49e617517215c308ccfc9b762c15489468dedf
moofazzel <mofazzel444.com>
2024-09-06 02:17:01 +0600

    Added categories section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import Banner from "@/components/Banner";
-import Features from "@/components/Features";
+import Banner from "@/app/_components/Banner";
+import Features from "@/app/_components/Features";
 import Navbar from "@/components/Navbar";
+import Categories from "./_components/Categories";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <Banner />
       <Features />
+      <Categories />
     </main>
   );
 }
