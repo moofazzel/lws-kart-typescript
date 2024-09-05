364d0d118804ec3172772a30f98b960443b9aca2
moofazzel <mofazzel444.com>
2024-09-06 02:03:22 +0600

    Features added

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Banner from "@/components/Banner";
+import Features from "@/components/Features";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <main>
       <Navbar />
       <Banner />
+      <Features />
     </main>
   );
 }
